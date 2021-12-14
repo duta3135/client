23e6d62b5ebe9171e9e513e6a8eb574d524775e2
duta3135 <dutasetyawan1012@gmail.com>
2021-12-14 14:33:27 +0700

    commit

---
+++ pages/admin/article-editor/index.js
@@ -0,0 +1,18 @@
+// import {useState} from 'react'
+// import {convertToRaw, EditorState } from 'draft-js';
+import TextEditor from '../../../components/TextEditor';
+function index() {
+
+    return (
+        <div>
+            <nav></nav>
+            <main>
+                <TextEditor
+                    
+                    />
+            </main>
+        </div>
+    )
+}
+
+export default index
