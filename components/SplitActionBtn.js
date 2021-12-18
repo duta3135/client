9e0bbcb2b7b6ab1b281f4768ab9e08f42f876467
duta3135 <dutasetyawan1012@gmail.com>
2021-12-18 10:56:48 +0700

    init -m "added split action button"

---
+++ components/SplitActionBtn.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SplitActionBtn() {
+    return (
+        <div>
+            <button>post</button>
+        </div>
+    )
+}
+
+export default SplitActionBtn
--- pages/admin/article-editor/index.js
+++ pages/admin/article-editor/index.js
@@ -1,12 +1,13 @@
 import EditorSidebar from "../../../components/EditorSidebar"
 import styles from "../../../styles/ArticleEditor.module.css"
+import SplitActionBtn from "../../../components/SplitActionBtn"
 function ArticleEditor() {
 
     return (
         <div className={styles.wrapper}>
             <EditorSidebar/>
             <main>
-                
+                <SplitActionBtn/>
             </main>
         </div>
     )
