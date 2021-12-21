d69bf447aeaccfcc5e039ca982c637af88dc03a5
duta3135 <dutasetyawan1012@gmail.com>
2021-12-21 12:04:17 +0700

    commit

--- components/SplitActionBtn.js
+++ components/SplitActionBtn.js
@@ -1,10 +1,16 @@
-import React from 'react'
-// import SplitButton from 'react-split-button'
-
+import React, {useState, useEffect} from 'react'
+import styles from '../styles/SplitActionBtn.module.css'
 function SplitActionBtn() {
+    const [dropdown, setDropdown] = useState(false)
+    const dropdownStyle = dropdown ? styles.dropdownOpen : styles.dropdownClosed
+    
     return (
-        <div>
-            <button>post</button>
+        <div className={styles.wrapper}>
+            <button className={styles.mainBtn}>Post to site</button>
+            <button className={styles.btnDown} onClick={console.log('clicked')}>
+                <svg fill='white' height='15' viewBox='0 0 24 20' width='15' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>
+            </button>
+            <div className={dropdownStyle}>Save to drafts</div>
         </div>
     )
 }
