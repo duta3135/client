bb67f6710fae2de141055f268f463058d3300cf7
duta3135 <dutasetyawan1012@gmail.com>
2021-12-30 14:50:03 +0700

    commit

--- pages/admin/signup.js
+++ pages/admin/signup.js
@@ -3,7 +3,7 @@ import styles from '../../styles/SignUp.module.css'
 import { useForm } from "react-hook-form";
 import axios from 'axios'
 function Signup() {
-    const { register, handleSubmit, formState: { errors } } = useForm();
+    const { register, handleSubmit, formState: { errors } , getValues} = useForm();
     const onSubmit = data =>{
         if(data.password === data.confirmPassword){
             
@@ -21,22 +21,30 @@ function Signup() {
               });
         }
         else{
-            alert('naw')
+            return        
         }
     };
+    const passwordMatched = getValues().password === getValues().confirmPassword ? <span></span> : <span>passwords dont match</span>
     return (
         <div className={styles.wrapper}>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <label>Name</label>
                 <input type="text" placeholder='bejo'{...register("name", {required: true})} />
+                {errors.name && <span>*name required</span>}
                 <label>username</label>
                 <input type="text" placeholder='bejo315'{...register("username", {required: true})} />
+                {errors.username && <span>*username required</span>}
+
                 <label>Password</label>
                 <input type="password"{...register("password", { required: true })} />
+                {errors.username && <span>*password required</span>}
                 <label>Confirm Password</label>
                 <input type="password" {...register("confirmPassword", { required: true })} />
+                {passwordMatched}
                 <label>instagram url</label>
                 <input type="url" {...register("insta", { required: true })} />
+                {errors.username && <span>*url required</span>}
+
                 <button type="submit">Sign-up</button>
             </form>
         </div>
