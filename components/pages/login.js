465c517027ce42db95522d50b42011733d096ac6
duta3135 <dutasetyawan1012@gmail.com>
2022-01-03 14:43:08 +0700

    commit

--- components/pages/login.js
+++ components/pages/login.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import {useForm} from 'react-hook-form'
+import styles from '../../styles/LogIn.module.css'
 function Login() {
+    const { register, handleSubmit, formState: { errors } , getValues} = useForm();
+    async function authenticate(data){
+        console.log(data)
+    }
     return (
-        <div>
-            <h1>login</h1>
+        <div className={styles.wrapper}>
+            <form onSubmit={handleSubmit(authenticate)}>
+                <input placeholder="johndoe95" type="text" {...register("username", {required:true})}/>
+                <input type="password" {...register("password", {required:true})}/>
+                <button type='submit'>Log in</button>
+            </form> 
         </div>
     )
 }
