a65430f68217ce560d51ae87f0ce0f45730e6ae7
duta3135 <dutasetyawan1012@gmail.com>
2022-01-05 08:05:00 +0700

    commit

--- components/pages/login.js
+++ components/pages/login.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import {useForm} from 'react-hook-form'
+import axios from 'axios'
 import styles from '../../styles/LogIn.module.css'
 function Login() {
     const { register, handleSubmit, formState: { errors } , getValues} = useForm();
     async function authenticate(data){
-        console.log(data)
+        axios.get(`http://localhost:3001/writers/${data.username}`)
+        .then(res=> console.log(res))
     }
     return (
         <div className={styles.wrapper}>
             <form onSubmit={handleSubmit(authenticate)}>
+                <label>Username</label>
                 <input placeholder="johndoe95" type="text" {...register("username", {required:true})}/>
+                <label>Password</label>
                 <input type="password" {...register("password", {required:true})}/>
                 <button type='submit'>Log in</button>
             </form> 
