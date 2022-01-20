809ac6f31766a904407290e909999ffe60eebeea
duta3135 <dutasetyawan1012@gmail.com>
2022-01-20 10:06:28 +0700

    commit -m "added cookies to article editor"

--- pages/admin/article-editor/index.js
+++ pages/admin/article-editor/index.js
@@ -4,9 +4,11 @@ import SplitActionBtn from "../../../components/SplitActionBtn"
 import { useState , useEffect} from "react"
 import dynamic from 'next/dynamic'
 import axios from 'axios'
+import { parseCookies } from '../../../helpers/parseCookies'
 
 const NoSSREditor = dynamic(()=> import('../../../components/TextEditor'), {ssr: false})
-function ArticleEditor({writers}) {
+function ArticleEditor({cookies, writers}) {
+    console.log(cookies)
     const [uploadStatus, setUploadStatus] = useState({isUploaded: false, url: ''})
     const [formState, setFormState] = useState({})
     const [textEditorState, setTextEditorState] = useState({})
@@ -48,16 +50,25 @@ function ArticleEditor({writers}) {
         </div>
     )
 }
-export async function getServerSideProps()
-{
-    try {
-        const res = await axios.get('http://localhost:3001/writers')
-        const writers = res.data
-        return{
-            props: {writers}
-        }
-    } catch (err) {
-        console.log(err)
+// export async function getServerSideProps()
+// {
+//     try {
+//         const res = await axios.get('http://localhost:3001/writers')
+//         const writers = res.data
+//         return{
+//             props: {writers}
+//         }
+//     } catch (err) {
+//         console.log(err)
+//     }
+// }
+ArticleEditor.getInitialProps = async ({req}) =>{
+    const cookies = parseCookies(req)
+    const res = await axios.get('http://localhost:3001/writers')
+    const writers = res.data
+    return {
+        cookies: cookies.user,
+        writers: writers
     }
 }
 export default ArticleEditor
