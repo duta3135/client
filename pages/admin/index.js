1125b47b9c042d8c1731d56cf3932d450e09fe86
duta3135 <dutasetyawan1012@gmail.com>
2022-02-02 16:18:35 +0700

    commit

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import Login from '../../components/pages/login'
 import { parseCookies } from '../../helpers/parseCookies'
 import Head from "next/head"
+import AdminLayout from '../../components/AdminLayout'
 
 export default function Index({cookies}) {
     if(cookies){
@@ -10,8 +11,9 @@ export default function Index({cookies}) {
                 <Head>
                     <title>Admin Page</title>
                 </Head>
-                hello
-                <pre>{cookies!==null?JSON.stringify(cookies):"no data"}</pre>
+                <AdminLayout>
+                    
+                </AdminLayout>
             </div>
         )
     }else{
