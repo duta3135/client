eed2e04e274eae691f660692a96d358dc8e7e867
duta3135 <dutasetyawan1012@gmail.com>
2022-02-08 13:26:16 +0700

    go to editor from admin page

--- components/AdminArticleCard.js
+++ components/AdminArticleCard.js
@@ -5,17 +5,23 @@ import styles from '../styles/AdminArticleCard.module.css'
 function AdminArticleCard({article}) {
   const status = article.published ? <div>published</div> : <div>draft</div>
   return (
-  <Link href={`/articles/${article._id}`}>
+  
     <div className={styles.wrapper}>
-        <img src={article.cover}></img>
+      <Link href={`/articles/${article._id}`}>
+        <img src={article.cover}></img></Link>
         <span>
         <h3>{article.title}</h3>
         by {article.writers}
         </span>
         <h4>{status}</h4>
-        
+        <a href={`/admin/article-editor/${article._id}`} >
+          <img src='https://res.cloudinary.com/duta3135/image/upload/v1644299955/edit_icon_mzmmm3.png' />
+        </a>
+        <button>
+          <img src='https://res.cloudinary.com/duta3135/image/upload/v1644299948/trashcan_icon_lzdydl.png'/>
+        </button>
     </div>
-  </Link>)
+  )
 }
 
 export default AdminArticleCard;
