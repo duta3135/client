681fc0cb4c63d85556bd01a75748ca269a97329e
duta3135 <dutasetyawan1012@gmail.com>
2022-02-11 08:33:55 +0700

    make landing page

--- components/ArticlesLayout.js
+++ components/ArticlesLayout.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import styles from '../styles/ArticlesPage.module.css'
+import axios from 'axios'
+import { useRouter } from 'next/router';
 
 export function ArticlesLayout({children}) {
+  const router = useRouter()
+  async function surpriseMe(){
+    axios.get('http://localhost:3001/articles?sample=true').then(res=>router.push(`/articles/${res.data[0]._id}`))
+  }
   return (
   <div>
       <header>
-        <a className={styles.navLinks} href='/entertainment'>Entertainment</a>
-        <a className={styles.navLinks} href='/food'>Food</a>
-        <a className={styles.navLinks} href='/health'>Health</a>
-        <a className={styles.navLinks} href='/politics'>Politics</a>
-        <button>Surpise me!</button>
+        <a className={styles.navLinks} href='/articles/entertainment'>Entertainment</a>
+        <a className={styles.navLinks} href='/articles/food'>Food</a>
+        <a className={styles.navLinks} href='/articles/health'>Health</a>
+        <a className={styles.navLinks} href='/articles/politics'>Politics</a>
+        <button onClick={()=>surpriseMe()}>Surpise me!</button>
       </header>
       {children}
   </div>)
