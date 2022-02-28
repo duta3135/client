4029b8ada295b9881b044a24aa0906faab0ba7f9
duta3135 <dutasetyawan1012@gmail.com>
2022-02-28 10:18:14 +0700

    changed modal triggers and copies

--- components/SplitActionBtn.js
+++ components/SplitActionBtn.js
@@ -17,7 +17,7 @@ function SplitActionBtn({publish, setModalState, title}) {
                 <svg fill='white' height='15' viewBox='0 0 24 20' width='15' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>
             </button>
             <div className={dropdownStyle} onClick={()=>setModalState({
-                text: `Post ${title} to site?`,
+                text: `save ${title} to drafts?`,
                 mainAction: ()=>publish(false),
                 show: true
             })}>Save to drafts</div>
